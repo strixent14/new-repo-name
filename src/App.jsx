94ed0d8ed1dad73ea6cb0d7399ce8f5554269841
Strixent14 <smeejaney@outlook.com>
2024-11-09 10:14:46 +0800

    Update app to send contact form submissions to email

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import React from 'react';
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './App.css'; 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Contact from './components/contact/Contact';
 import Review from './components/review/Review';
-import Pricing from "./components/Pricing/Pricing"; // Correct import for Pricing
+import Pricing from "./components/pricing/Pricing"; 
 
 const App = () => {
   return (
     <HashRouter>
       <Header />
-      <main className='main'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} /> {/* Updated route for Pricing */}
-          <Route path="/contact" element={<Contact />} /> {/* Contact route */}
-          <Route path="/review" element={<Review />} />
-        </Routes>
-      </main>
+      <Main />
     </HashRouter>
+  );
+}
+
+const Main = () => {
+  const location = useLocation();
+
+  return (
+    <main className='main'>
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          classNames="fade"
+          timeout={300}
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/review" element={<Review />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+    </main>
   );
 }
 
